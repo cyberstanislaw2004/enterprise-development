be37f7d06c65991472767366eff65b1bf98e8c25
cyberstanislaw2004 <cyberstanislaw2004@gmail.com>
2025-09-16 19:07:31 +0400

    added classes "Passenger" and "Ticket"

---
+++ Airlines/Airlines.Domain/Passenger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airlines.Domain;
+
+/// <summary>
+/// Класс, характеризующий пассажира
+/// </summary>
+public class Passenger
+{
+    /// <summary>
+    /// Уникальный ID для праймари ключа в БД
+    /// </summary>
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Номер паспорта
+    /// </summary>
+    public required string NumberOfPasspotr { get; set; }
+
+    /// <summary>
+    /// ФИО
+    /// </summary>
+    public required string FullName { get; set; }
+
+    /// <summary>
+    /// Дата рождения
+    /// </summary>
+    public required DateOnly BirthDate { get; set; }
+}
---
+++ Airlines/Airlines.Domain/Ticket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Airlines.Domain;
+
+/// <summary>
+/// Класс, характеризующий билет
+/// </summary>
+public class Ticket
+{
+    /// <summary>
+    /// Уникальный ID для праймари ключа в БД
+    /// </summary>
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Информация о рейсе
+    /// </summary>
+    public required Flight FlightInfo { get; set; }
+
+    /// <summary>
+    /// Информация о пассажире
+    /// </summary>
+    public required Passenger PassengerInfo { get; set; }
+
+    /// <summary>
+    /// Номер сидения
+    /// </summary>
+    public required string SeatNumber { get; set; }
+
+    /// <summary>
+    /// Наличие ручной клади
+    /// </summary>
+    public required bool? HandLuggageAvailability { get; set; } // 0 - no; 1 - yes
+
+    /// <summary>
+    /// Суммарный вес багажа
+    /// </summary>
+    public required double TotalBaggageWeight { get; set; }
+}
