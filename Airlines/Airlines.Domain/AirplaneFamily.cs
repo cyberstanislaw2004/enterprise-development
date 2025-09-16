c14c949921d82492be2d5d224a6e3375b9719ca7
cyberstanislaw2004 <cyberstanislaw2004@gmail.com>
2025-09-16 18:51:09 +0400

    added class "AirplaneFamily"

--- Airlines/Airlines.Domain/AirplaneFamily.cs
+++ Airlines/Airlines.Domain/AirplaneFamily.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace Airlines.Domain;
 
+/// <summary>
+/// Класс, описывающий семейство самолетов
+/// </summary>
 public class AirplaneFamily
 {
+    /// <summary>
+    /// Название семейства
+    /// </summary>
+    public required string NameOfFamily { get; set; }
+
+    /// <summary>
+    /// Название производителя
+    /// </summary>
+    public required string NameOfManufacturer { get; set; }
 }
