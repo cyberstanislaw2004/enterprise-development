c2d92283650efb871f7fee25588367381356bb24
cyberstanislaw2004 <cyberstanislaw2004@gmail.com>
2025-11-10 10:21:15 +0400

    API added

---
+++ Airlines/WebApplication1/Controllers/AirplaneFamilyController.cs
@@ -0,0 +1,50 @@
+﻿using Airlines.Dto;
+using Airlines.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Airlines.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AirplaneFamilyController : ControllerBase
+{
+    private readonly AirplaneFamilyService _service;
+
+    public AirplaneFamilyController(AirplaneFamilyService service) =>
+        _service = service;
+
+    [HttpGet]
+    public IActionResult GetAll() =>
+        Ok(_service.GetAirplaneFamilies());
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var entity = _service.GetAirplaneFamily(id);
+        if (entity == null) return NotFound();
+        return Ok(entity);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] AirplaneFamilyCreateDto dto)
+    {
+        var id = _service.CreateAirplaneFamily(dto);
+        return CreatedAtAction(nameof(Get), new { id }, dto);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, [FromBody] AirplaneFamilyCreateDto dto)
+    {
+        var updated = _service.UpdateAirplaneFamily(id, dto);
+        if (updated == null) return NotFound();
+        return Ok(updated);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var deleted = _service.DeleteAirplaneFamily(id);
+        if (!deleted) return NotFound();
+        return NoContent();
+    }
+}
---
+++ Airlines/WebApplication1/Program.cs
@@ -0,0 +1,47 @@
+using Airlines.Application.Services;
+using Airlines.Domain;
+using Airlines.Domain.Repositories;
+using Airlines.Domain.Dataseeder;
+using Airlines.Infrastructure.InMemory.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddSingleton<Dataseeder>();
+builder.Services.AddSingleton<IRepository<AirplaneFamily>, InMemoryAirplaneFamilyRepository>();
+builder.Services.AddSingleton<IRepository<AirplaneModel>, InMemoryAirplaneModelRepository>();
+builder.Services.AddSingleton<IRepository<Flight>, InMemoryFlightRepository>();
+builder.Services.AddSingleton<IRepository<Passenger>, InMemoryPassengerRepository>();
+builder.Services.AddSingleton<IRepository<Ticket>, InMemoryTicketRepository>();
+
+builder.Services.AddScoped<AirplaneFamilyService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var basePath = AppContext.BaseDirectory;
+
+    options.IncludeXmlComments(Path.Combine(basePath, "Airlines.Domain.xml"));
+    options.IncludeXmlComments(Path.Combine(basePath, "Airlines.Api.xml"));
+    options.IncludeXmlComments(Path.Combine(basePath, "Airlines.Application.xml"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
