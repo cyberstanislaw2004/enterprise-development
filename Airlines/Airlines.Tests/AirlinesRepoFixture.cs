c49e53b171bd4d66eb623007bfe1582288199b68
cyberstanislaw2004 <cyberstanislaw2004@gmail.com>
2025-11-10 11:41:33 +0400

    tests added

---
+++ Airlines/Airlines.Tests/AirlinesRepoFixture.cs
@@ -0,0 +1,32 @@
+﻿using Airlines.Application.Services;
+using Airlines.Infrastructure.InMemory.Repositories;
+using Airlines.Domain.Dataseeder;
+
+namespace Airlines.Tests;
+
+public class AirlinesRepoFixture
+{
+    public AirplaneFamilyService AirplaneFamilyService { get; }
+    public AirplaneModelService AirplaneModelService { get; }
+    public FlightService FlightService { get; }
+    public PassengerService PassengerService { get; }
+    public TicketService TicketService { get; }
+
+    public AirlinesRepoFixture()
+    {
+        var dataseeder = new Dataseeder();
+
+        var airplaneFamilyRepository = new InMemoryAirplaneFamilyRepository(dataseeder);
+        var airplaneModelRepository = new InMemoryAirplaneModelRepository(dataseeder);
+        var flightRepository = new InMemoryFlightRepository(dataseeder);
+        var passengerRepository = new InMemoryPassengerRepository(dataseeder);
+        var ticketRepository = new InMemoryTicketRepository(dataseeder);
+
+
+        AirplaneFamilyService = new AirplaneFamilyService(airplaneFamilyRepository);
+        AirplaneModelService = new AirplaneModelService(airplaneModelRepository);
+        FlightService = new FlightService(flightRepository);
+        PassengerService = new PassengerService(passengerRepository);
+        TicketService = new TicketService(ticketRepository);
+    }
+}
---
+++ Airlines/Airlines.Tests/AirlinesRepoTests.cs
@@ -0,0 +1,144 @@
+﻿namespace Airlines.Tests;
+
+public class AirlinesRepoTests(AirlinesRepoFixture fixture) : IClassFixture<AirlinesRepoFixture>
+{
+    //private readonly AirlinesRepoFixture _fixture;
+
+    [Fact]
+    public void GetTopFiveFlights_WhenFlightsExist_ReturnsFlightsInDescendingPassengerCount()
+    {
+        var flights = fixture.FlightService.GetFlights();
+        var tickets = fixture.TicketService.GetTickets();
+
+        var topFive = (
+            from flight in flights
+            let passengerCount =
+                (from ticket in tickets
+                 where ticket.FlightInfo == flight
+                 select ticket).Count()
+            orderby passengerCount descending
+            select new
+            {
+                Flight = flight.FlightNumber,
+                PassengerCount = passengerCount
+            })
+            .Take(5)
+            .ToList();
+
+        Assert.Equal(5, topFive.Count());
+        for (var i = 0; i < topFive.Count() - 1; i++) Assert.True(topFive[i].PassengerCount >= topFive[i + 1].PassengerCount);
+    }
+
+    /// <summary>
+    /// Display a list of flights with the minimum travel time.
+    /// </summary>
+    [Fact]
+    public void GetFlightsWithMinDuration_WhenFlightsExist_ReturnsAllWithMinDuration()
+    {
+        var flights = fixture.FlightService.GetFlights();
+
+        var minDuration = (
+            from flight in flights
+            where flight.Duration == flights.Min(f => f.Duration)
+            select new
+            {
+                Flight = flight.FlightNumber,
+                Duration = flight.Duration
+            })
+            .ToList();
+
+        Assert.NotEmpty(minDuration);
+
+        var expectedMinDuration = flights.Min(f => f.Duration);
+
+        for (var i = 0; i < minDuration.Count(); i++) Assert.True(minDuration[i].Duration == expectedMinDuration);
+    }
+
+    /// <summary>
+    /// Display information about all passengers flying on the selected flight
+    /// whose baggage weight is zero, sorted by full name.
+    /// </summary>
+    [Fact]
+    public void GetPassengersWithZeroBaggage_OnSelectedFlight_ReturnsPassengersOrderedByFullName()
+    {
+        var flights = fixture.FlightService.GetFlights();
+        var tickets = fixture.TicketService.GetTickets();
+
+        var selectedFlight = flights.First(f => f.FlightNumber == "SU100");
+
+        var infoAboutPassenger = (
+            from ticket in tickets
+            where ticket.TotalBaggageWeight == 0
+                && ticket.FlightInfo.FlightNumber == selectedFlight.FlightNumber
+            orderby ticket.PassengerInfo.FullName
+            select ticket.PassengerInfo)
+            .ToList();
+
+        Assert.NotEmpty(infoAboutPassenger);
+
+        foreach (var passenger in infoAboutPassenger)
+        {
+            Assert.Equal(selectedFlight.FlightNumber, tickets.First(t => t.PassengerInfo.Id == passenger.Id).FlightInfo.FlightNumber);
+            Assert.Equal(0, tickets.First(t => t.PassengerInfo.Id == passenger.Id).TotalBaggageWeight);
+        }
+
+        for (var i = 0; i < infoAboutPassenger.Count() - 1; i++) Assert.True(string.Compare(infoAboutPassenger[i].FullName, infoAboutPassenger[i + 1].FullName) <= 0);
+    }
+
+    /// <summary>
+    /// Display summary information about all flights of aircraft 
+    /// of the selected model during a specified period of time.
+    /// </summary>
+    [Fact]
+    public void GetFlightsByModelWithinPeriod_WhenFlightsExist_ReturnsMatchingFlightNumbers()
+    {
+        var flights = fixture.FlightService.GetFlights();
+
+        var selectedModel = "Il-96-300";
+        var startDate = new DateOnly(2025, 10, 10);
+        var endDate = new DateOnly(2025, 12, 5);
+
+        var allFlights = (
+            from flight in flights
+            where flight.AirplaneModel.ModelName == selectedModel
+                && flight.DepartureDate >= startDate
+                && flight.ArrivalDate <= endDate
+            select flight.FlightNumber)
+            .ToList();
+
+        var expectedData = new[]
+        {
+            "SU400",
+            "SU900"
+        };
+
+        Assert.Equal(expectedData, allFlights);
+    }
+
+    /// <summary>
+    /// Display information about all flights departing from a specified
+    /// departure point to a specified arrival point.
+    /// </summary>
+    [Fact]
+    public void GetFlightsByDepartureAndDestination_WhenFlightsExist_ReturnsMatchingFlightNumbers()
+    {
+        var flights = fixture.FlightService.GetFlights();
+
+        var startSpot = "SVO";
+        var endSpot = "LED";
+
+        var allFlights = (
+            from flight in flights
+            where flight.DepartureAirportCode == startSpot
+                && flight.DestinationAirportCode == endSpot
+            select flight.FlightNumber).ToList();
+
+        var expectedData = new[]
+        {
+            "SU100",
+            "SU101"
+        };
+
+        Assert.Equal(expectedData, allFlights);
+    }
+}
